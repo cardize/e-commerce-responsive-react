3e76ebc0d5ed40c35bc6976927bb421848991e98
cardize <mustafasamisakarya@gmail.com>
2022-03-20 20:06:52 +0300

    pages template completed

--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
-export default function checkoutPage() {
+//import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+//import StripeButton from '../../components/stripe-button/stripe-button.component'
+//import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+
+import './CheckOutPageStyles.scss'
+
+function CheckoutPage({ cartItems, total }) {
   return (
-    <div>
-      <h1>Check out</h1>
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Products</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      <div className="total">
+        <span>Total= ${total}</span>
+      </div>
+      <div className="test-warning">
+        Please use any data for address and name and email, also use <br />
+        card number: 4242 4242 4242 4242, expiry: 02/22, cvv: 123 for dummy
+        payment
+      </div>
+      <button price={total} />
     </div>
   )
 }
+
+export default CheckoutPage
