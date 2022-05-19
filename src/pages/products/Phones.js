a837e4c093c6f0091e35cf66a5207dbaa1774379
cardize <mustafasamisakarya@gmail.com>
2022-05-20 01:55:04 +0300

    rename

--- src/pages/products/Phones.js
+++ src/pages/products/Phones.js
@@ -6,18 +6,18 @@ import { addToCart, removeFromCart, cartTotal } from '../../redux/actions/index'
 function Phones(props) {
   return (
     <div className="productsContainer">
-      {props.phones.map((phone) => (
-        <div className="productContainer" key={phone.id}>
-          <img className="productImage" src={phone.Img} alt="" />
+      {props.phones.map((item) => (
+        <div className="productContainer" key={item.id}>
+          <img className="productImage" src={item.Img} alt="" />
           <h1 className="productName">
-            {phone.name} {phone.capacity}GB
+            {item.name} {item.capacity}GB
           </h1>
-          <h2 className="price">${phone.price}</h2>
+          <h2 className="price">${item.price}</h2>
           <div className="addToCartButtonContainer">
             <button
               className="addToCartButton"
               onClick={() => (
-                props.addToCart(phone), props.cartTotal(phone.price)
+                props.addToCart(item), props.cartTotal(item.price)
               )}
             >
               Add to Cart
